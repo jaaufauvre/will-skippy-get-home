91bee6666cd2606e97972338fb06e88479e15232
Aufauvre, Jean-Alexis <jean-alexis.aufauvre@outlook.com>
2020-09-06 17:32:58 +0100

    Addressed Rubocop warnings (part 2), added main

--- ruby/lib/skippy.rb
+++ ruby/lib/skippy.rb
@@ -5,23 +5,25 @@ require 'skippy/grid'
 require 'skippy/kangaroo'
 
 module Skippy
-  # Create a grid
-  puts('Enter dimension of the grid (>=1):')
-  dimension = gets.to_i
-  grid = Grid.new(dimension)
+  def self.main
+    # Create a grid
+    puts('Enter dimension of the grid (>=1):')
+    dimension = gets.to_i
+    grid = Grid.new(dimension)
 
-  # Place Skippy on the grid
-  skippy = Kangaroo.new
-  grid.place_item(skippy, Point.new(0, 0))
+    # Place Skippy on the grid
+    skippy = Kangaroo.new
+    grid.place_item(skippy, Point.new(0, 0))
 
-  # Place Skippy's home on the grid
-  home = GridItem.new
-  grid.place_item(home, Point.new(dimension - 1, dimension - 1))
+    # Place Skippy's home on the grid
+    home = GridItem.new
+    grid.place_item(home, Point.new(dimension - 1, dimension - 1))
 
-  # Ask skippy to find his home
-  skippy.find_home(grid, home)
+    # Ask skippy to find his home
+    skippy.find_home(grid, home)
 
-  # Print die statistics
-  puts ''
-  Die.instance.print_stats
+    # Print die statistics
+    puts ''
+    Die.instance.print_stats
+  end
 end
